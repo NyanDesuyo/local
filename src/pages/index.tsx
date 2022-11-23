cb0ab22d6f62a8936e2917247934f41478458936
NyanDesuyo <gerogeroe12@gmail.com>
2022-11-23 08:34:09 +0700

    update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const getAllQuery = trpc.useQuery([
-    "post.detail",
-    {
-      id: "this is id",
-    },
-  ]);
+  const { data, isError, isLoading } = trpc.useQuery(["post.all"]);
 
-  if (getAllQuery.isLoading) {
+  if (isLoading) {
     return (
       <div className="h-screen w-screen flex flex-col justify-center">
         <p className="text-center">Loading....</p>
@@ -18,7 +14,7 @@ const Home: NextPage = () => {
     );
   }
 
-  if (getAllQuery.isError) {
+  if (isError) {
     return (
       <div className="h-screen w-screen flex flex-col justify-center">
         <p className="text-center">Something went Wrong, check Project</p>
@@ -28,9 +24,30 @@ const Home: NextPage = () => {
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center">
+      <Link href={"/post"} className="content-center">
+        Post!
+      </Link>
+      <br />
       <h1 className="text-center">This is from TRPC:</h1>
-      <p className="text-center">{getAllQuery.data?.message}</p>
-      <p className="text-center">{JSON.stringify(getAllQuery.data?.data)}</p>
+      <p className="text-center">{data?.message}</p>
+      <br />
+      {data?.result.map(
+        ({ id, docid, create_at, update_at, title, body, isPosted }, index) => {
+          return (
+            <div className="text-center" key={docid}>
+              <p className="">No: {index + 1}</p>
+              <br />
+              <p className="">Create At: {create_at.toString()}</p>
+              <p className="">Update At: {update_at.toString()}</p>
+              <br />
+              <p className="">Title: {title}</p>
+              <p className="">Body: {body}</p>
+              <p className="">Posted: {isPosted ? "✔" : "❌"}</p>
+              <br />
+            </div>
+          );
+        }
+      )}
     </div>
   );
 };
